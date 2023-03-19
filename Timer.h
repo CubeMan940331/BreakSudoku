48b4f299a40e772fae8cafd85b81271fe302377d
CubeMan9403331 <CubeMAn940331@gmail.com>
2023-03-20 00:23:04 +0800

    lib

---
+++ Timer.h
@@ -0,0 +1,18 @@
+#ifndef CubeMan_Timer_
+#define CubeMan_Timer_
+#include<chrono>
+namespace cubeman{
+class Timer{
+private:
+    std::chrono::time_point<std::chrono::steady_clock> t1,t2;
+public:
+    void start(){
+        t1=std::chrono::steady_clock::now();
+    }
+    long long elapsed_us(){
+        t2=std::chrono::steady_clock::now();
+        return std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
+    }
+};
+}
+#endif
